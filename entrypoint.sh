3144022b95b14d6c45b5901b3640efc3aee07e2e
Mikko Partio <mikko.partio@fmi.fi>
2023-10-20 08:44:11 +0300

    Forked from https://github.com/addnab/docker-run-action
    Replace docker with podman

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+then echo $INPUT_PASSWORD | podman login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+if [ ! -z $INPUT_PODMAN_NETWORK ];
+then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_PODMAN_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec podman run $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
